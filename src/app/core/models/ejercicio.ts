1822f3e57f60b70e67ca79fe2213e71394a940f0
agar777 <90040171+agar777@users.noreply.github.com>
2023-03-19 18:36:59 -0400

    quitando template

--- src/app/core/models/ejercicio.ts
+++
@@ -1,8 +0,0 @@
-export class Ejercicio {
-  id: string;
-  nombre: string;
-  descripcion: string;
-  repeticiones: number;
-  series: number;
-  descanso: number;
-}
--- src/app/interfaces/ejercicio.interface.ts
+++
@@ -1,9 +0,0 @@
-export default interface ejercicio{
-    id?: string;
-    nombre: string;
-    descripcion: string;
-    repeticiones: number;
-    series: number;
-    descanso: number;
-    imagen: string;
-}
